6516605722092d3d51faea6dce4826b64df3de77
SAIGUSA Tomotada <ts@nectarition.jp>
2024-08-01 16:04:04 +0900

    新デザイン作成

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,9 +5,18 @@ const pageCollection = defineCollection({
     draft: z.boolean().optional(),
     title: z.string(),
     date: z.date(),
+    isMetaPage: z.boolean().optional(),
     showMenu: z.boolean().optional(),
     menuTitle: z.string().optional(),
-    menuOrder: z.number().optional()
+    menuOrder: z.number().optional(),
+    menuIcon: z.string().optional(),
+    menuSection: z.array(
+      z.object({
+        name: z.string(),
+        path: z.string(),
+        backgroundImage: z.string()
+      }))
+      .optional(),
   })
 })
 
